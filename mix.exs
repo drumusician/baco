b05b7cd2b3d0fc31f96c3d15a7cdf5479dfd98b6
Tjaco Oostdijk <info@drumusician.com>
2018-11-16 10:23:55 +0100

    add more info and exdoc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Baco.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      source_url: "https://github.com/drumusician/baco",
+      package: package()
     ]
   end
 
@@ -21,8 +24,23 @@ defmodule Baco.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19.1"}
     ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Tjaco Oostdijk"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/drumusician/baco"
+      }
+    ]
+  end
+
+  defp description do
+    """
+    A Useful tool to use inside your Mix projects
+    """
   end
 end
