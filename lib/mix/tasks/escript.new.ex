27daac8bee2d7b0d9898ef8832599381baec08c4
Tjaco Oostdijk <info@drumusician.com>
2018-11-18 00:04:19 +0100

    add api and task generator

---
+++ lib/mix/tasks/escript.new.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.Escript.New do
+  @moduledoc """
+  Create a new Mix project that is setup to create an escript
+  """
+
+  def run(_args) do
+    IO.puts "Sorry, not implemented yet..."
+  end
+end
